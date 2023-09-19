c4d9a8d04f93f2f11744b05d6758ec4d8ff05e83
yhonlaverde <yhonlaverde0@gmail.com>
2023-09-19 11:12:37 -0500

    home list notes V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import {CommonModule} from '@angular/common'
-import { NgModule } from '@angular/core'
-import { Routes } from '@angular/router'
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { NotesComponent } from './components/notes/notes.component';
 
-
-const ROUTES: Routes =[
-
-]
+const ROUTES: Routes = [];
 
 @NgModule({
-    declarations:[],
-    imports:[],
-    providers:[],
-    exports:[],
-    bootstrap:[]
+  declarations: [NotesComponent],
+  imports: [],
+  providers: [],
+  exports: [],
+  bootstrap: [],
 })
-
 export class AppModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    MatToolbarModule, 
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule],
   templateUrl: './home.component.html',
-  // styleUrls: ['./home.component.css'],
+ styleUrls: ['./home.component.scss'],
+ 
 })
-export class HomeComponent {}
+export class HomeComponent {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+}
