94c7b15e4036a644b399da1ad80c55199fb49e7d
yhonlaverde <yhonlaverde0@gmail.com>
2023-09-20 11:51:13 -0500

    form validations check

--- my-lib/src/lib/components/createNote/create-note.component.ts
+++ my-lib/src/lib/components/createNote/create-note.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'lib-create-note',
   templateUrl: './create-note.component.html',
   styleUrls: ['./create-note.component.css'],
 })
 export class CreateNoteComponent {
-  note: string | undefined;
+  note!: string;
 
+  constructor(private router: Router,  private activatedRoute: ActivatedRoute) {
+   
+  }
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   saveNote():void{
 
   }
+
+
+  returnHome():void{
+    this.router.navigate(['/home']);
+  }
+
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent } ,
+  { path: 'home', component: HomeComponent } ,
   {path: 'note', loadChildren: () => import('my-lib/src/lib/my-lib.module').then((m) =>m.MyLibModule)},
      
 ];
